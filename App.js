bb930da6e46c5a512485b1cf0744f8c5d855feeb
Palmito254 <bgr921@myy.haaga-helia.fi>
2022-09-12 21:57:59 +0300

    f

--- App.js
+++ App.js
@@ -1,11 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, FlatList } from 'react-native';
 
 export default function App() {
+
+  const [text, setText] = useState("");
+  const [data, setData] = useState([]);
+
+  const add = () => {
+    setData([...data, {key: text}]);
+    setText("")
+  }
+
+  const clear = () => {
+    setData([])
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text></Text>
+      <Text></Text>
+      <Text></Text>
       <StatusBar style="auto" />
+      <TextInput 
+        style={{ borderColor: "gray", borderWidth: 1, width: 200}}
+        onChangeText={text => setText(text)}
+        value={text}
+      />
+      <View style={{flex: 0.2, flexDirection: "row", alignItems: "center", justifyContent: "space-between"}}>
+        <Button onPress={add} title="Add" />
+        <Text>       </Text>
+        <Button onPress={clear} title="Clear" />
+      </View>
+      <Text>Shopping List</Text>
+      <FlatList
+        data={data}
+        renderItem={({item}) => <Text>{item.key}</Text>}
+        keyExtractor={(item, index) => index.toString()}
+      />
     </View>
   );
 }
@@ -15,6 +47,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
   },
 });
